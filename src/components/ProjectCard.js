c3f7b9b67abbabd9af0ea3b6a210417ad7e424ab
Henry Bonilla <ing.henry.bonilla@gmail.com>
2023-12-12 16:37:40 -0500

    Projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,16 +4,17 @@ export const ProjectCard = ({ title, desc, imgUrl }) => {
     return (
         <Col sm={6} md={4}>
             <div className="proj-imgbox">
-                <img src={imgUrl} alt="Imagen de viaualización del proyecto" />
+                <img src={imgUrl} />
+                <div className="proj-txt">
+                    <h4>
+                        {title}
+                    </h4>
+                    <span>
+                        {desc}
+                    </span>
+                </div>
             </div>
-            <div className="proj-txt">
-                <h4>
-                    {title}
-                </h4>
-                <span>
-                    {desc}
-                </span>
-            </div>
+
         </Col>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,29 +1,30 @@
 import { Col, Container, Row } from "react-bootstrap"
 import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
 import { ProjectCard } from "./ProjectCard";
+import img_test from "../assets/images/developer_banner.png"
+import Nav from 'react-bootstrap/Nav';
 
 export const Projects = () => {
     const projects = [
         {
             title: "Proyecto1",
             desc: "Descripcion del proyecto",
-            imgUrl: "../assets/images/developer_banner.png"
+            imgUrl: img_test
         },
         {
             title: "Proyecto 2",
             desc: "Descripcion del proyecto",
-            imgUrl: "../assets/images/paisaje-bosque-niebla-3d.jpg"
+            imgUrl: img_test
         },
         {
             title: "Proyecto 3",
             desc: "Descripcion del proyecto",
-            imgUrl: ""
+            imgUrl: img_test
         },
         {
             title: "Proyecto 4",
             desc: "Descripcion del proyecto",
-            imgUrl: ""
+            imgUrl: img_test
         },
     ]
     return (
@@ -33,27 +34,42 @@ export const Projects = () => {
                     <Col>
                         <h2>Proyectos</h2>
                         <p>Descripción</p>
-                        <Tabs
-                            defaultActiveKey="first"
-                            id="uncontrolled-tab-example"
-                            className="mb-3"
-                        >
-                            <Tab eventKey="first" title="1">
-                                {
-                                    projects.map((project, index) => {
-                                        return (
-                                            <ProjectCard key={index}{...project}></ProjectCard>
-                                        )
-                                    })
-                                }
-                            </Tab>
-                            <Tab eventKey="second" title="2">
-                                Tab content for 2
-                            </Tab>
-                            <Tab eventKey="third" title="3">
-                                Tab content for 3
-                            </Tab>
-                        </Tabs>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">1</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">2</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return (
+                                                    <ProjectCard key={index}{...project}></ProjectCard>
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return (
+                                                    <ProjectCard key={index}{...project}></ProjectCard>
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                            </Tab.Content>
+
+
+                        </Tab.Container>
                     </Col>
                 </Row>
             </Container>
