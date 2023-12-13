af036183474bafb864f19007c788478e857281f9
Henry Bonilla <ing.henry.bonilla@gmail.com>
2023-12-12 20:29:35 -0500

    Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,7 @@
 import { useState } from "react"
-import { Col } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
+import ReCAPTCHA from "react-google-recaptcha";
+import image from "../assets/images/developer_banner.png";
 
 export const Contact = () => {
     const form = {
@@ -13,6 +15,7 @@ export const Contact = () => {
     const [formDetails, setFormDetails] = useState(form);
     const [buttonText, setButtonText] = useState('Enviar');
     const [status, setStatus] = useState({});
+    const [captchaIsDone, setCaptchaIsDone] = useState(false);
 
     const onFormUpdate = (atr, value) => {
         setFormDetails({
@@ -21,15 +24,36 @@ export const Contact = () => {
         })
     }
 
-    const submit = () => {
+    function onChange(){
+        console.log("ChangeDetected")
+        setCaptchaIsDone(true)
+    }
 
+    const submit = async (e) => {
+        e.preventDefault();
+        setButtonText('Enviando...');
+        let response = await fetch("https://prod-09.westus.logic.azure.com:443/workflows/23b3ba7c788948faa588c69094f914c8/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Wf40Iafa9sh9xZVqEY6GxjMgOTElgC9ydwnBjCAl6u0",
+            {
+                method: "POST",
+                headers: { "Content-Type": "Application/json" },
+                body: JSON.stringify(form),
+            }
+        );
+        setButtonText('Enviado')
+        let result = response.json();
+        setFormDetails(form)
+        if (result.code === 200) {
+            setStatus({ success: true, message: "El mensaje fue enviado." })
+        } else {
+            setStatus({ success: false, message: "Hubo un problema, Si el problema persiste no dudes en contactarme por LinkedIn." })
+        }
     }
 
     return (
         <section className="contact" id="contact">
             <Row className="align-items-center">
                 <Col md={6}>
-                    <img src="" />
+                    <img src={image} />
                 </Col>
                 <Col md={6}>
                     <h2>
@@ -51,11 +75,15 @@ export const Contact = () => {
                             </Col>
                             <Col sm={6} className="px-1">
                                 <textarea row="6" value={form.message} placeholder="Mensaje" onChange={(e) => { onFormUpdate('message', e.target.value) }} />
-                                <button type="submit" 
-                                    class="g-recaptcha"
-                                    data-sitekey="reCAPTCHA_site_key"
-                                    data-callback='onSubmit'
-                                    data-action='submit'>
+                            </Col>
+                            <Col sm={12} className="px-1">
+                                <ReCAPTCHA
+                                    sitekey="6Lc-ly8pAAAAAA10FbglaPO62tigmF2CV58_nreP"
+                                    onChange={onChange}
+                                />
+                            </Col>
+                            <Col sm={12} className="px-1">
+                                <button className="btn btn-primary" type="submit" disabled={!captchaIsDone}>
                                     <span>
                                         {buttonText}
                                     </span>
@@ -64,7 +92,7 @@ export const Contact = () => {
                             {
                                 status.message &&
                                 <Col>
-                                    <p className={status.success === false ? "danger" : success}>
+                                    <p className={status.success === false ? "danger" : "success"}>
                                         {status.message}
                                     </p>
                                 </Col>
